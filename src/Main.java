4386d5f92e5960789529f8cab5b5023fccfd8647
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-10-05 19:02:25 +0530

    Box class Kunal OOP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
+
+/*
+-----------Inheritance--------------
+->Just like inheritance in real life
+->Like money and property and face and other properties they pass it into children and children can
+    use those things
+->child class and base class
+->child class has access to properties of base class
+->child class is inheriting property from the base class.
+->class childClass extends BaseClass {
+    }
+->In construcutores you have to initialze parent class variables also
+ */
---
+++ src/inheritance/Main.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box=new Box();
+        System.out.println(box.l+" "+box.w+" "+box.h);
+    }
+}

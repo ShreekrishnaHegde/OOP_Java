823b307a53dcf7b769a23ac42b0bb08257827947
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-10-12 12:58:05 +0530

    Singleton class

--- src/OOPBasics/InnerClass.java
+++ src/OOPBasics/InnerClass.java
@@ -24,7 +24,7 @@ public class InnerClass {
 ->Where it is mentioned as static,
 -----Reason for the output-----------
 ->Class Test is declared as static which is inside the InnerClass. Hence, Class test is not dependent on
-    the objects of InnerClass. Hence you can access class Test without creating an object of InnerClass.
+    the objects of InnerClass. Hence, you can access class Test without creating an object of InnerClass.
 ->But Test can have its own objects, but static here means class Test is independent of instances of the
     "InnerClass".
 ->Static methods/variables are resolved during compile time.
--- src/OOPBasics/Singleton.java
+++ src/OOPBasics/Singleton.java
@@ -1,7 +1,22 @@
 package OOPBasics;
 
 public class Singleton {
+
+    private Singleton(){
+
+    }
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null)
+            instance=new Singleton();
+        return instance;
+    }
+
     public static void main(String[] args) {
+        Singleton obj=Singleton.getInstance();
+        Singleton obj1=Singleton.getInstance();
+        //All above reference variables are pointing to the same object.
 
     }
 }
